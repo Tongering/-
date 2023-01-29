7c11e1da8d937b98dcf9215d2b0d4f768e38a598
同ætongering <wzhengshimao@sina.cn>
2023-01-29 17:31:20 +0800

    implement web acwing oauth2 login

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -570,6 +570,9 @@ class Settings {
         this.$register_login = this.$register.find(".ac-game-settings-option");
 
         this.$register.hide();
+
+        this.$acwing_login = this.$settings.find('.ac-game-settings-acwing img');
+
         this.root.$ac_game.append(this.$settings);
 
         this.start();
@@ -581,8 +584,12 @@ class Settings {
     }
 
     add_listening_events() {
+        let outer = this;
         this.add_listening_events_login();
         this.add_listening_events_register();
+        this.$acwing_login.click(function(){
+            outer.acwing_login();
+        })
     }
 
     add_listening_events_login() {
@@ -602,6 +609,19 @@ class Settings {
         });
         this.$register_submit.click(function() {
             outer.register_on_remote();
+        })
+    }
+
+    acwing_login() {
+        $.ajax({
+            url: "https://app3206.acapp.acwing.com.cn/settings/acwing/web/apply_code/",
+            type: "GET",
+            success: function(resp) {
+                console.log(resp);
+                if(resp.result === "success") {
+                    window.location.replace(resp.apply_code_url);
+                }
+            }
         })
     }
 
